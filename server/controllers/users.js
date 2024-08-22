95716319594a62eca2ffbd5d4a1207a97e41aa58
lawrencejr5 <oputalawrence@gmail.com>
2024-08-22 17:30:23 +0100

    basic server resgistration

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,9 +1,30 @@
 const User = require("../models/users");
+
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   try {
-    res.status(200).json({ msg: "Success" });
+    const { username, fullname, phone, password } = req.body;
+
+    if (!username || !fullname || !phone || !password)
+      res.status(501).json({ msg: "username or email wrong" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    const tempUser = { username, fullname, phone, password: hashedPass };
+    const user = await User.create({ ...tempUser });
+
+    const token = jwt.sign(
+      { userId: user._id, username },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_LIFETIME,
+      }
+    );
+
+    res.status(200).json({ user: user._id, token });
   } catch (err) {
     console.log(err);
   }
