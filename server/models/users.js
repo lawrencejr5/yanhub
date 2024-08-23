fa72f82aeabced568f8224bdc5f8f4192a3ecdcf
lawrencejr5 <oputalawrence@gmail.com>
2024-08-23 17:44:51 +0100

    set up some mongoose function

--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema(
   {
@@ -42,6 +44,17 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.methods.createJWt = function () {
+  return jwt.sign({ userId: user._id, username }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+userSchema.methods.comparePass = async function (pass) {
+  const isMatch = await bcrypt.compare(pass, this.password);
+  return isMatch;
+};
 
 const userModel = mongoose.model("Users", userSchema);
 module.exports = userModel;
