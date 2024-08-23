f54b28193fe73c8001107c53852bc04e7f564089
lawrencejr <oputalawrence@gmail.com>
2024-08-23 06:48:47 +0100

    user reg and login

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,15 +7,24 @@ const register = async (req, res) => {
   try {
     const { username, fullname, phone, password } = req.body;
 
-    if (!username || !fullname || !phone || !password)
-      res.status(501).json({ msg: "username or email wrong" });
+    // Check required fields
+    if (!username || !fullname || !password)
+      return res.status(501).json({ msg: "Fill in required fields" });
 
+    // Check if user exists
+    const oldUser = await User.findOne({ username });
+    if (oldUser)
+      return res.status(501).json({ msg: "Username already exists" });
+
+    // Hashing password
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(password, salt);
 
-    const tempUser = { username, fullname, phone, password: hashedPass };
-    const user = await User.create({ ...tempUser });
+    // Storing user in db
+    const userData = { username, fullname, phone, password: hashedPass };
+    const user = await User.create({ ...userData });
 
+    // Creating token
     const token = jwt.sign(
       { userId: user._id, username },
       process.env.JWT_SECRET,
@@ -24,6 +33,7 @@ const register = async (req, res) => {
       }
     );
 
+    // Sending response
     res.status(200).json({ user: user._id, token });
   } catch (err) {
     console.log(err);
@@ -32,7 +42,33 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    res.status(200).json({ msg: "Success" });
+    const { username, password } = req.body;
+
+    // Check required fields
+    if (!username || !password)
+      return res.status(501).json({ msg: "Fill in required fields" });
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) return res.status(500).json({ msg: "User does not exist" });
+
+    // Checking if password matches
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(500).json({ msg: "Password not correct" });
+    }
+
+    // Creating token
+    const token = jwt.sign(
+      { userId: user._id, username },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_LIFETIME }
+    );
+
+    // Sending response
+    res
+      .status(200)
+      .json({ msg: "Successfully signed in", user: user._id, token });
   } catch (err) {
     console.log(err);
   }
