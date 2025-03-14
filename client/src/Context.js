c99002e733dfc9f6f5c7bc75646608229d16601a
lawrencejr5 <oputalawrence@gmail.com>
2025-03-14 17:25:06 +0100

    vercel config

--- client/src/Context.js
+++ client/src/Context.js
@@ -97,7 +97,7 @@ export const Context = ({ children }) => {
   };
 
   // Endpoint and token
-  const endpoint = "https://yanhub.onrender.com/api/v1";
+  const endpoint = "https://yanhub.vercel.app/api/v1";
   // const endpoint = "http://localhost:5001/api/v1";
   const token = localStorage.getItem("token");
 
